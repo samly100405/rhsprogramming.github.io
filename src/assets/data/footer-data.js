c2f926c83f5e64a68f2b87c93eee7f1cb6c43a4c
Sam Ly <derpythesquid12@gmail.com>
2022-08-25 00:21:33 -0700

    intro animation, darkened background

---
+++ src/assets/data/footer-data.js
@@ -0,0 +1,27 @@
+export default { // UNUSED
+  title: "RHS Programming",
+  officers: [
+    {
+      name: "MK Tan",
+      position: "President",
+    },
+    {
+      name: "Yunzhi Lin",
+      position: "VP of Activities"
+    },
+    {
+      name: "Sam Ly",
+      position: "VP of Competition"
+    },
+    {
+      name: "Henry Do",
+      position: "Treasurer"
+    },
+    {
+      name: "Collin Chen",
+      position: "Secretary"
+    },
+
+
+  ]
+}
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div className="section" id="footer">
+
+    </div>
+  )
+}
+
+// UNUSED
